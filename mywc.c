d604a19ebd3e4a9dd3ff402704d04bddae6dd966
Kathilist <kli8772@students.fhv.at>
2024-10-21 09:57:36 +0000

    The function is improved and the feature "-h" was added.

--- mywc.c
+++ mywc.c
@@ -7,12 +7,25 @@
 int main(int argc, char *argv[]) {
     int count = 0;
 
-    if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'c') {
-        while (getchar() != EOF) {
-            count++;
+    if (argc == 2 && argv[1][0] == '-') {
+        switch (argv[1][1]) {
+            case 'h':
+                printf("This app can count inputs in two ways:\n"
+                       "-c\tCounts the characters\n"
+                       "-w\tCounts the words\n");
+            case 'c':
+                while (getchar() != EOF) {
+                    count++;
+                }
+                printf("\nNumber of characters: %d\n", count);
+                return 0;
+            case 'w':
+                printf("The new feature will be executed!\n");
         }
-        printf("\nNumber of characters: %d\n", count);
-        return 0;
+    } else {
+        printf("ERROR: wrong input\n"
+               "use one of the following commands:\n"
+               "'-c' or '-c' or '-w'\n");
     }
     
     return 0;
